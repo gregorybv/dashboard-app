ae5d1aff7e1b9728a904cc891dc4cd64b37fb709
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-02-10 13:16:06 +0300

    data add

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,10 @@ export default function Home() {
       <main className='bg-gray-100 min-h-screen'>
         <Header/>
         <TopCards/>
-        <BarChar/>
-        <RecentOrders/>
+        <div className='p-4 grid md:grid-cols-3 grid-cols-1 gap-4'>
+          <BarChar/>
+          <RecentOrders/>
+        </div>
       </main>
     </>
   )
